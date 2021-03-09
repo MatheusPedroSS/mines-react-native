82cac5edc8fa7db999f74a29e5a879e6f5c3419b
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-09 10:46:52 -0300

    Componente MineField

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import params from './src/params';
-import Field from './src/components/Field'
+import MineField from './src/components/MineField'
+import { createMinedBoard } from './src/functions'
 
 export default class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = this.createState()
+  }
+
+  minesAmount = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return Math.ceil(cols * rows * params.difficultLevel)
+  }
+
+  createState = () => {
+    const cols = params.getColumnsAmount()
+    const rows = params.getRowsAmount()
+    return {
+      board: createMinedBoard(rows, cols, this.minesAmount()),
+    }
+  }
+
   render() {
     return (
       <SafeAreaView style={styles.container}>
@@ -11,17 +32,9 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           Tamanho da grade:{params.getRowsAmount()}X{params.getColumnsAmount()}
         </Text>
-        <Field />
-        <Field opened />
-        <Field opened nearMines={1}/>
-        <Field opened nearMines={2}/>
-        <Field opened nearMines={3}/>
-        <Field opened nearMines={6}/>
-        <Field mined />
-        <Field mined opened />
-        <Field mined opened exploded />
-        <Field flagged />
-        <Field flagged opened />
+        <View style={styles.board}>
+          <MineField board={this.state.board} />
+        </View>
       </SafeAreaView>
     )
   }
@@ -30,15 +43,10 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-end'
+  },
+  board: {
     alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10
-  },
-  instructions: {
-    
+    backgroundColor: '#AAA'
   }
 })
