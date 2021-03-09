7aa703c8bd7f57426246d0d12a1ecadf74f23998
Matheus Pedro <matheus.pedro1214@gmail.com>
2021-03-09 08:50:13 -0300

    Componente Field #01

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { SafeAreaView, StyleSheet, Text } from 'react-native'
 import params from './src/params';
+import Field from './src/components/Field'
 
 export default class App extends Component {
   render() {
@@ -10,6 +11,7 @@ export default class App extends Component {
         <Text style={styles.instructions}>
           Tamanho da grade:{params.getRowsAmount()}X{params.getColumnsAmount()}
         </Text>
+        <Field />
       </SafeAreaView>
     )
   }
@@ -17,7 +19,8 @@ export default class App extends Component {
 
 const styles = StyleSheet.create({
   container: {
-
+    flex: 1,
+    alignItems: 'center',
   },
   welcome: {
 
